59a706787d9145d7468543c9fab261d79d73a3a4
ksnirmala <97504107+ksnirmala@users.noreply.github.com>
2022-04-11 15:30:53 +0530

    Add files via upload

---
+++ AssignOperator.java
@@ -0,0 +1,19 @@
+package Assignment;
+
+public class AssignOperator {
+	public static void main(String args[]) {
+		int a=10,b=9,c=5,d=20,e;
+		a+=5;
+		System.out.println(a);
+		b-=5;
+		System.out.println(b);
+		c*=5;
+		System.out.println(c);
+		d/=5;
+		System.out.println(d);
+		e=a+b+c+d;
+		System.out.println(e);
+		
+	}
+
+}
