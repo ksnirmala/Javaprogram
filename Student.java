1fe844c19611cfb543ccbfef1fd180cce0fc4864
ksnirmala <97504107+ksnirmala@users.noreply.github.com>
2022-04-11 15:52:04 +0530

    Add files via upload

--- Student.java
+++ Student.java
@@ -1,41 +1,41 @@
-package collections;
-
-import java.util.ArrayList;
-import java.util.Iterator;
-
-class Students{
-	int rollno;
-	String sname;
+package oopsconcepts;
+import java.util.Scanner;
+import java.lang.*;
+class student{
+	String name;
+	int id;
 	float fees;
-	public Students(int i,String string, int j) {
-		rollno=i;
-		sname=string;
-		fees=j;
-		
+	char gen;
+	int age;
+
+void input() {
+	Scanner sc=new Scanner(System.in);
+	System.out.println("enter name,id,fees,gen,age of a student");
+	name=sc.nextLine();
+	id=sc.nextInt();
+	fees=sc.nextFloat();
+	gen=sc.next().charAt(0);
+	age=sc.nextInt();
+}
+	
+	void display() {
+		System.out.println("name="+name);
+		System.out.println("id="+id);
+		System.out.println("fees="+fees);
+		System.out.println("gen="+gen);
+		System.out.println("age="+age);
 	}
+		
 }
 
 public class Student {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Students ob1=new Students(1,"nirmala",23000);
-		Students ob2=new Students(2,"siva",50000);
-		ArrayList<Students> a=new ArrayList<>();
-		a.add(ob1);
-		a.add(ob2);
-		System.out.println(a);
-		Iterator<Students> it=a.iterator();
-		System.out.println("rollno\tsname\tfees");
-		while(it.hasNext()) {
-			Students a1=(Students) it.next();
-			System.out.println(a1.rollno+"\t"+a1.sname+"\t"+a1.fees+"\t\t");
-			System.out.println();
-			
-		}
-		
-		
-		
+		student sob=new student();
+		System.out.println(sob);
+		sob.input();
+		sob.display();
 		
 
 	}
