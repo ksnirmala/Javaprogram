9ba6881bb685eb6f6487bd2708cc116681c2c1e1
ksnirmala <97504107+ksnirmala@users.noreply.github.com>
2022-04-11 15:55:16 +0530

    Add files via upload

---
+++ LogicOperator.java
@@ -0,0 +1,19 @@
+package logical;
+
+public class LogicOperator {
+	public static void main(String args[]) {
+		int a=9,b=10,c=5;
+		boolean x,y,z;
+		x=(a>b&&a>c);
+		y=(a>b||a>c);
+		z=!(a>10);
+		System.out.println(x);
+		System.out.println(y);
+		System.out.println(z);
+		
+		
+		
+		
+		
+	}
+}
