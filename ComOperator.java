bb1d28ae25d38ad365e6471365458928fc529638
ksnirmala <97504107+ksnirmala@users.noreply.github.com>
2022-04-11 15:39:03 +0530

    Add files via upload

---
+++ ComOperator.java
@@ -0,0 +1,21 @@
+package compare;
+
+public class ComOperator {
+	public static void main(String args[]) {
+		int a=10,b=6;
+		boolean c;
+		c=(a>b);
+		System.out.println(c);
+		c=(a<b);
+		System.out.println(c);
+		c=(a>=b);
+		System.out.println(c);
+		c=(a<=b);
+		System.out.println(c);
+		c=(a==b);
+		System.out.println(c);
+		
+		
+	}
+
+}
